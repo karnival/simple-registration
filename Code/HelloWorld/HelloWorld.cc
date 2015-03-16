6b27b6740d1c8ac97d0c491051084b88044ee391
Matt Clarkson <m.clarkson@ucl.ac.uk>
2015-03-16 14:10:22 +0000

    Added example of how to link to boost

--- Code/HelloWorld/HelloWorld.cc
+++ Code/HelloWorld/HelloWorld.cc
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include <boost/lexical_cast.hpp>
+#include <boost/program_options.hpp>
 #include <itkImage.h>
 
 int main(int argc, char** argv)
@@ -30,6 +31,12 @@ int main(int argc, char** argv)
 
   // Check Boost is correctly included.
   std::cout << "The number 10.0 converted to string is " << boost::lexical_cast<std::string>(10.0) << std::endl;
+
+  // Check Boost libraries are correctly included.
+  boost::program_options::options_description desc("Allowed options");
+  desc.add_options()
+      ("help", "produce help message")
+  ;
 
   // Check ITK is correctly included.
   typedef itk::Image<int, 2> ImageType;
