e21ddd4e76889d406b7351f9665fba2431342527
David Owen <david.owen.14@ucl.ac.uk>
2016-06-15 19:07:30 +0100

    Factor out pointset functions and rename pointsets.

--- Code/PointMatching/PointMatching.cc
+++ Code/PointMatching/PointMatching.cc
@@ -3,45 +3,42 @@
 #include <Eigen/Dense>
 #include <Eigen/SVD>
 
+Eigen::Vector3d find_pointset_average(const Eigen::MatrixXd& pointset) {
+    auto average = pointset.rowwise().mean();
+    return average;
+}
+
+Eigen::MatrixXd residuals_from_average(const Eigen::MatrixXd& pointset, const Eigen::Vector3d& point) {
+    return pointset.colwise() - point;
+}
+
+void estimate_rigid_transform(const Eigen::MatrixXd& pointset, const Eigen::MatrixXd& pointset_dash) {
+    auto p = find_pointset_average(pointset);
+    auto p_dash = find_pointset_average(pointset_dash);
+
+    auto q = residuals_from_average(pointset, p);
+    auto q_dash = residuals_from_average(pointset_dash, p_dash);
+
+    auto H = q * q_dash.transpose();
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
+
+    auto proposed_rotation = svd.matrixV()*(svd.matrixU()).transpose();
+
+    auto translation = p_dash - proposed_rotation*p;
+}
+
 int main(void) {
-    Eigen::MatrixXd p_set(3,4);
-    p_set << 1, 3, 1, 3,
+    Eigen::MatrixXd pointset(3,4);
+    pointset << 1, 3, 1, 3,
              2, 2, 5, 5,
              3, 3, 3, 3;
 
-    Eigen::MatrixXd p_dash_set(3,4);
-    p_dash_set << 0.28, 1.70,-1.83,-0.41,
+    Eigen::MatrixXd pointset_dash(3,4);
+    pointset_dash << 0.28, 1.70,-1.83,-0.41,
                   2.13, 3.54, 4.26, 5.66,
                   3.00, 3.00, 3.00, 3.01;
     
-    auto p = (p_set.rowwise().mean());
-    auto p_dash = (p_dash_set.rowwise().mean());
-
-    std::cout << "p is " << p << std::endl;
-    std::cout << "p' is " << p_dash << std::endl;
-
-    Eigen::Matrix3d H; 
-    H.setZero();
-
-    for(int i = 0; i < p_set.cols(); i++) {
-        H += (p_set.col(i) - p) * (p_dash_set.col(i) - p_dash).transpose();
-    }
-
-    std::cout << "H is " << H << std::endl;
-
-    Eigen::JacobiSVD<Eigen::MatrixXd> svd(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
-    std::cout << "Lambda is " << svd.singularValues() << std::endl;
-    std::cout << "U is " << svd.matrixU() << std::endl;
-    std::cout << "V is " << svd.matrixV() << std::endl;
-
-    auto X = svd.matrixV()*(svd.matrixU()).transpose();
-
-    std::cout << "X is " << X << std::endl;
-    std::cout << "X's det is " << X.determinant() << std::endl;
-
-    auto T = p_dash - X*p;
-
-    std::cout << "T is " << T << std::endl;
-
+    estimate_rigid_transform(pointset, pointset_dash);
     return 0;
 }
