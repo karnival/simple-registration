4f2fb5f20730840fc11381c9a9da7c2419b2d717
David Owen <david.owen.14@ucl.ac.uk>
2016-06-15 18:13:00 +0100

    Find rotation matrix.

--- Code/PointMatching/PointMatching.cc
+++ Code/PointMatching/PointMatching.cc
@@ -22,5 +22,24 @@ int main(void) {
     std::cout << "p is " << p << std::endl;
     std::cout << "p' is " << p_dash << std::endl;
 
+    Eigen::Matrix3d H; 
+    H.setZero();
+
+    for(int i = 0; i < p_set.rows(); i++) {
+        H += (p_set.row(i) - p).transpose() * (p_dash_set.row(i) - p_dash);
+    }
+
+    std::cout << "H is " << H << std::endl;
+
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(H, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    std::cout << "Lambda is " << svd.singularValues() << std::endl;
+    std::cout << "U is " << svd.matrixU() << std::endl;
+    std::cout << "V is " << svd.matrixV() << std::endl;
+
+    auto X = svd.matrixV()*(svd.matrixU()).transpose();
+
+    std::cout << "X is " << X << std::endl;
+    std::cout << "X's det is " << X.determinant() << std::endl;
+
     return 0;
 }
