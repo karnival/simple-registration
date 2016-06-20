7fa5c9dbf9f29b1bfc6d9af66a3e14148410e6b6
David Owen <david.owen.14@ucl.ac.uk>
2016-06-20 18:20:55 +0100

    Parse the filename options and print them back.

--- Code/PointMatching/PointMatchingCmd.cc
+++ Code/PointMatching/PointMatchingCmd.cc
@@ -10,13 +10,17 @@ int main(int argc, char** argv) {
     try {
         std::string appName = boost::filesystem::basename(argv[0]);
 
+        std::string data1;
+        std::string data2;
+        std::string out;
+
         namespace opts = boost::program_options;
         opts::options_description desc("Options");
         desc.add_options()
                 ("help", "Print help message")
-                ("data1", "First point cloud")
-                ("data2", "Second point cloud")
-                ("out", "Output filename")
+                ("data1", opts::value<std::string> (&data1)->required(), "First point cloud filename.")
+                ("data2", opts::value<std::string> (&data2)->required(), "Second point cloud filename.")
+                ("out", opts::value<std::string> (&out)->required(), "Output filename.")
         ;
 
         opts::positional_options_description positionalOptions;
@@ -42,8 +46,18 @@ int main(int argc, char** argv) {
 
             return 1;                                  
         }                                                                
+
+        if(vm.count("data1")) {
+            std::cout << "data1 was " << vm["data1"].as<std::string>() << std::endl;
+        }
           
+        if(vm.count("data2")) {
+            std::cout << "data2 was " << vm["data2"].as<std::string>() << std::endl;
+        }
        
+        if(vm.count("out")) {
+            std::cout << "out was " << vm["out"].as<std::string>() << std::endl;
+        }
       
     } catch(std::exception& e) {
         std::cerr << "Unhandled Exception reached the top of main: " 
